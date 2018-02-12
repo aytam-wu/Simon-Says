97a56332631c4641aba72c494c29226c25e09706
Ai Yen Tam <tam.aiyen@gmail.com>
2018-02-12 15:05:22 -0500

    action

---
+++ script.js
@@ -0,0 +1,10 @@
+var greenBtn = document.querySelector('#circle').addEventListener('blink', blinkGreen)
+
+function blinkSequence () {
+
+
+}
+
+var redBtn = document.querySelector('#circle2')
+var yellowBtn = document.querySelector('#circle3')
+var blueBtn = document.querySelector('#circle4')
