c3b53429907351fd5911a9771409bec2beee71fb
Ai Yen Tam <tam.aiyen@gmail.com>
2018-02-13 14:03:41 -0500

    reverse opacity

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
-var greenBtnEnter = document.querySelector('#circle').addEventListener('mouseenter', function () {
-    document.getElementById('circle').style.opacity = '0.5';
-})
+// mouse entering and leaving - opacity
 
-var greenBtnLeave = document.querySelector('#circle').addEventListener('mouseleave', function () {
+var greenBtnEnter = document.querySelector('#circle').addEventListener('mouseenter', function () {
     document.getElementById('circle').style.opacity = '1';
 })
 
-var redBtnEnter = document.querySelector('#circle2').addEventListener('mouseenter', function () {
-    document.getElementById('circle2').style.opacity = '0.5';
+var greenBtnLeave = document.querySelector('#circle').addEventListener('mouseleave', function () {
+    document.getElementById('circle').style.opacity = '0.5';
 })
 
-var redBtnLeave = document.querySelector('#circle2').addEventListener('mouseleave', function () {
+var redBtnEnter = document.querySelector('#circle2').addEventListener('mouseenter', function () {
     document.getElementById('circle2').style.opacity = '1';
 })
 
-var yellowBtnEnter = document.querySelector('#circle3').addEventListener('mouseenter', function () {
-    document.getElementById('circle3').style.opacity = '0.5';
+var redBtnLeave = document.querySelector('#circle2').addEventListener('mouseleave', function () {
+    document.getElementById('circle2').style.opacity = '0.5';
 })
 
-var yellowBtnLeave = document.querySelector('#circle3').addEventListener('mouseleave', function () {
+var yellowBtnEnter = document.querySelector('#circle3').addEventListener('mouseenter', function () {
     document.getElementById('circle3').style.opacity = '1';
 })
 
+var yellowBtnLeave = document.querySelector('#circle3').addEventListener('mouseleave', function () {
+    document.getElementById('circle3').style.opacity = '0.5';
+})
+
 var blueBtnEnter = document.querySelector('#circle4').addEventListener('mouseenter', function () {
-    document.getElementById('circle4').style.opacity = '0.5';
+    document.getElementById('circle4').style.opacity = '1';
 })
 
 var blueBtnLeave = document.querySelector('#circle4').addEventListener('mouseleave', function () {
-    document.getElementById('circle4').style.opacity = '1';
+    document.getElementById('circle4').style.opacity = '0.5';
 })
+
+// game sequence & speed
+
 
