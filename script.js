a3934fccaa0fa17d72b885c9a39122c5ce31d22d
Ai Yen Tam <tam.aiyen@gmail.com>
2018-02-12 21:04:27 -0500

    mouse enter/leave, opacity

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-var greenBtn = document.querySelector('#circle').addEventListener('blink', blinkGreen)
+var greenBtnEnter = document.querySelector('#circle').addEventListener('mouseenter', function () {
+    document.getElementById('circle').style.opacity = '0.5';
+})
 
-function blinkSequence () {
+var greenBtnLeave = document.querySelector('#circle').addEventListener('mouseleave', function () {
+    document.getElementById('circle').style.opacity = '1';
+})
 
+var redBtnEnter = document.querySelector('#circle2').addEventListener('mouseenter', function () {
+    document.getElementById('circle2').style.opacity = '0.5';
+})
 
-}
+var redBtnLeave = document.querySelector('#circle2').addEventListener('mouseleave', function () {
+    document.getElementById('circle2').style.opacity = '1';
+})
 
-var redBtn = document.querySelector('#circle2')
-var yellowBtn = document.querySelector('#circle3')
-var blueBtn = document.querySelector('#circle4')
+var yellowBtnEnter = document.querySelector('#circle3').addEventListener('mouseenter', function () {
+    document.getElementById('circle3').style.opacity = '0.5';
+})
+
+var yellowBtnLeave = document.querySelector('#circle3').addEventListener('mouseleave', function () {
+    document.getElementById('circle3').style.opacity = '1';
+})
+
+var blueBtnEnter = document.querySelector('#circle4').addEventListener('mouseenter', function () {
+    document.getElementById('circle4').style.opacity = '0.5';
+})
+
+var blueBtnLeave = document.querySelector('#circle4').addEventListener('mouseleave', function () {
+    document.getElementById('circle4').style.opacity = '1';
+})
+
