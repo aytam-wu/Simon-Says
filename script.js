6a1559593183eea60471490b78c5950b446a788b
Ai Yen Tam <tam.aiyen@gmail.com>
2018-02-15 00:45:00 -0500

    buttons and score

--- script.js
+++ script.js
@@ -3,59 +3,15 @@ var userInput = [];
 var currentRound = 0;
 var score = 0;
 var sequence = 0;
+var started = true;
+var quit = false;
 
-// start sequence button
-var start = document.getElementById('start').addEventListener('click', function () {
-    roundStart();
-});
-
-// starting sequence
-var roundStart = function() {
-    var circles = document.getElementsByClassName('button');
-    if(score < 1){
-        for(var i = 0; i < circles.length; i++){
-            circles[i].addEventListener('click', function(){
-                var circleId = this.getAttribute('id'); // this circle
-                userInput.push(parseInt(circleId.charAt(circleId.length - 1))); // grabbing last user input's circle array
-                roundStart(); // running program again
-            });
-        }
-    }
-
-// scoreboard
-    if(score === 0){
-        simonSaysArray.push(Math.floor(Math.random()*4)+1);
-        simonSays(simonSaysArray); // flashing circles
-        score++; // score increments
-    }
-// checking userInput and simonSaysArray
-    if(userInput.length === simonSaysArray.length){
-        for(var i = 0; i < userInput.length; i++){
-            if(JSON.stringify(userInput) === JSON.stringify(simonSaysArray)){  // array becomes a string
-                userInput = [];
-                simonSaysArray.push(Math.floor(Math.random()*4)+1);
-                simonSays(simonSaysArray); // simonSaysArray takes the array and flashs circles
-                score++;
-                roundStart(); // runs program again
-                break;
-            }
-            // checking userInput and simonSaysArray if they do not match up
-            if(userInput[i] !== simonSaysArray[i]){
-                userInput = [];
-                simonSays = [];
-                score = 0;
-                alert('You lose')
-                break;
-            }
-        }
-    }
-}
 // setting interval
 var simonSays = function(simonSaysArray) {
     var x = 0
     var flashColor = setInterval( // flashing circles
         function(){
-            $('#circle' + simonSaysArray[x]).fadeTo('fast', 0).fadeTo('fast', 1);
+            $('#circle' + simonSaysArray[x]).fadeTo('fast', 0.5).fadeTo('fast', 1);
             x++;
             if(x >= simonSaysArray.length){
                 clearInterval(flashColor)
@@ -63,3 +19,64 @@ var simonSays = function(simonSaysArray) {
         }, 1000
     )
 }
+
+// starting sequence
+var roundStart = function() {
+    var circles = document.getElementsByClassName('button');
+    if(started && !quit){
+        for(var i = 0; i < circles.length; i++){
+            circles[i].addEventListener('click', function(){
+                var circleId = this.getAttribute('id'); // this circle
+                userInput.push(parseInt(circleId.charAt(circleId.length - 1))); // grabbing last user input's circle array
+                roundStart(); // running program again
+            });
+        }
+        quit = true;
+    }
+
+// checks if the game has started
+    if(started){
+        simonSaysArray.push(Math.floor(Math.random()*4)+1);
+        simonSays(simonSaysArray); // flashing circles
+        started = false;
+    }
+// checking userInput and simonSaysArray
+    for(var i = 0; i < userInput.length; i++){
+        if(JSON.stringify(userInput) === JSON.stringify(simonSaysArray)){  // array becomes a string
+            userInput = [];
+            simonSaysArray.push(Math.floor(Math.random()*4)+1);
+            simonSays(simonSaysArray); // simonSaysArray takes the array and flashs circles
+            score++;
+            var scoretext = document.getElementById('score');
+            scoretext.innerText = '' + score;
+            roundStart(); // runs program again
+            break;
+        }
+        // checking userInput and simonSaysArray if they do not match up
+        if(userInput[i] !== simonSaysArray[i]){
+            userInput = [];
+            simonSaysArray = [];
+            score = 0;
+            var scoretext = document.getElementById('score');
+            scoretext.innerText = '' + score;
+            started = true;
+            alert('You lose')
+            break;
+        }
+    }
+}
+
+// start sequence button
+var start = document.getElementById('start').addEventListener('click', function () {
+    roundStart();
+});
+
+// quit button
+var quit = document.getElementById('quit').addEventListener('click', function () {
+    userInput = [];
+    simonSaysArray = [];
+    score = 0;
+    var scoretext = document.getElementById('score');
+    scoretext.innerText = '' + score;
+    started = true;
+});
